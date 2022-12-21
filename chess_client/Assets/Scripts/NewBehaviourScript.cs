a58e842e63607479f64a8809685628b274dd2f27
Valerazz2 <107555043+Valerazz2@users.noreply.github.com>
2022-12-21 20:44:43 +0200

    init

---
+++ chess_client/Assets/Scripts/NewBehaviourScript.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using TCom;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    private Wind wnd = Wind.North;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ chess_server/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace chess_server.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
